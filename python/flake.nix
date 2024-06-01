4bee5a747d3a963eb58579c8e4f8b83da44b6421
heyzec <61238538+heyzec@users.noreply.github.com>
2024-06-01 17:06:38 +0800

    feat: add example of fetching from pypi

--- python/flake.nix
+++ python/flake.nix
@@ -18,6 +18,17 @@
           python-packages = ps: with ps; [
             pandas
             requests
+            (let
+              pname = "openlocationcode";
+              version = "1.0.1";
+            in pkgs.python3Packages.buildPythonPackage {
+              inherit pname version;
+              src = pkgs.fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-b8AQioIUtl10lkEFvWlkWop1KSN/Deaq3PqDzDNzs1k=";
+              };
+              doCheck = false;
+            })
           ];
         in python3.withPackages python-packages)
       ];
