ae6b2356864d67318a42644fc15eef402454204c
heyzec <61238538+heyzec@users.noreply.github.com>
2024-06-02 02:15:11 +0800

    feat(python): update to use poetry

--- python/flake.nix
+++ python/flake.nix
@@ -1,38 +1,26 @@
 {
-  description = "Template for a direnv shell, with Python";
+  description = "Template for a direnv shell, with Python + Poetry";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, poetry2nix }:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-
+    inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+    myPythonApp = mkPoetryApplication { projectDir = ./.; };
   in
   {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
-        (let
-          python-packages = ps: with ps; [
-            pandas
-            requests
-            (let
-              pname = "openlocationcode";
-              version = "1.0.1";
-            in pkgs.python3Packages.buildPythonPackage {
-              inherit pname version;
-              src = pkgs.fetchPypi {
-                inherit pname version;
-                sha256 = "sha256-b8AQioIUtl10lkEFvWlkWop1KSN/Deaq3PqDzDNzs1k=";
-              };
-              doCheck = false;
-            })
-          ];
-        in python3.withPackages python-packages)
+        poetry
+        myPythonApp
       ];
     };
   };
 }
+
 
